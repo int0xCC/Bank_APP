8550f58d5336938835a9ab24e9eda4bc5f916bc1
0xCC <155317937+int0xCC@users.noreply.github.com>
2024-01-09 22:27:26 +0530

    Create sm_int.py

---
+++ sm_int.py
@@ -0,0 +1,13 @@
+print("Type in the principle amount: ")
+p = int(input())
+
+print("Type in the rate of interest: ")
+r = float(input())
+
+print("Type in the time period: ")
+t = float(input())
+
+si = (p*r*t)/100
+
+print("\nTada! The simple interest is: ")
+print(si)
